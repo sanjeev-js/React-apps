0f9910af538f096df4ae22b389bb7fa6934aa078
sanjeev8386 <sanjeev18@navgurukul.org>
2019-09-01 18:16:02 +0530

    Weather

--- weather-app/src/form.js
+++ weather-app/src/form.js
@@ -1,11 +1,19 @@
-import React from 'react';
-const Form = (props) => {
-  return (
-      <form onSubmit={props.loadWeather}>
-        <input type="text" name="city" placeholder="City..." />
-        <input type="text" name="country" placeholder="Country..." />
-        <button>Get Weather</button>
-      </form>
-  )
+import React from "react";
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { TemperatureOfDelhi: 36, TemperatureOfMumbai: 34 };
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.state.TemperatureOfDelhi}>
+          <input type="text" name="city" placeholder="City..." />
+          <input type="text" name="country" placeholder="Country..." />
+          <button>Get Weather</button>
+        </form>
+      </div>
+    );
+  }
 }
-export default Form;
--- weather-app/src/weather.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Weather = props => {
-  return 
-  <div>
-    //weather conditions goes here
-  </div>;
-};
-export default Weather;

6d57fa9b5ab38527ea0c4ca2e5b5e8c5730d0427
sanjeev8386 <sanjeev18@navgurukul.org>
2019-09-04 08:12:41 +0530

    implemented edit in todo

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,69 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { EADDRNOTAVAIL } from "constants";
 
 class TodoApp extends React.Component {
-    constructor (props) {
-        super (props);
-        this.state = {items: [], text: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: "", editItemId: null };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-render() {
+  render() {
     return (
-        <div>
-            <h3>TODO</h3>
-            <TodoList items = {this.state.items} />
-            <form onSubmit = {this.handleSubmit} >
-            <label>What's needs to be Do?</label>
-            <input onChange = {this.handleChange} value = {this.state.text} />
-            <button>ADD </button>
-            </form>
-        </div>
-        );
-    }
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} handleEdit={this.handleEdit} />
+        <form onSubmit={this.handleSubmit}>
+          <label>What's needs to be Do?</label>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button>ADD </button>
+        </form>
+      </div>
+    );
+  }
 
-    handleChange (e) {
-        this.setState ({text: e.target.value});
-    }
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
 
-    handleSubmit (e) {
-        e.preventDefault();
-        if (! this.state.text.length) {
-            return;
-        }
-        const newItem = {
-            text : this.state.text,
-            id: Date.now()
-        };
-        this.setState(state => ({
-            items: state.items.concat(newItem),
-            text: ''
-        }));
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
     }
+    if (this.state.editItemId !== null) {
+      var elem = this.state.items.find(
+        element => this.state.editItemId === element.id
+      );
+      var elemIndex = this.state.items.indexOf(elem);
+      var newItems = this.state.items.slice();
+      var newItem = {
+        ...elem,
+        text: this.state.text
+      };
 
-    handleEdit = (e) => {
+      newItems[elemIndex] = newItem;
+      this.setState({ items: newItems });
+    } else {
+      const newItem = {
+        text: this.state.text,
+        id: Date.now()
+      };
+      this.setState(state => ({
+        items: state.items.concat(newItem),
+        text: ""
+      }));
     }
+  }
 
-    handleComplete = (e) => {
+  handleEdit = (e, id) => {
+    var editableItem = this.state.items.find(element => element.id === id);
+    console.log(editableItem);
+    this.setState({ text: editableItem.text, editItemId: editableItem.id });
+  };
 
-    }
+  handleComplete = e => {};
 }
 
 class TodoList extends React.Component {
-    render() {
-        return (
-            <ul>
-            {this.props.items.map(item => (
-            <div>
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <div>
             <li key={item.id}>{item.text}</li>
-            <button onClick = {this.handleEdit}>Edit</button>
-            <button onClick = {this.handleComplete}>Complete</button>
-            </div>
-            ))}
-            </ul>
-        );
-    }
+            <button onClick={e => this.props.handleEdit(e, item.id)}>
+              Edit
+            </button>
+            <button onClick={this.handleComplete}>Complete</button>
+          </div>
+        ))}
+      </ul>
+    );
+  }
 }
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById("root"));

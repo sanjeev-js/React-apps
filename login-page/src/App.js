00963b377ce0dd258f1b56dfea730f11c5e3a4c9
sanjeev8386 <sanjeev18@navgurukul.org>
2019-09-05 15:35:49 +0530

    integrated backend api

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -4,10 +4,10 @@ import "./App.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { email: "", password: "" };
+    this.state = { email: "", password: "" , auth_token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21lciI6eyJlbWFpbCI6InNhbmplZXYxOEBnbWFpbC5jb20iLCJwYXNzd29yZCI6InBhc3N3b3JkIn0sImlhdCI6MTU2NzY2MDY3NywiZXhwIjoxNTY3NzQ3MDc3fQ.NGlbxSWt3c4nrFvctGsy4IQNQBFXajdWSVCO0fxIf3o"};
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleLogin.bind(this);
   }
 
   handleChange(event) {
@@ -18,11 +18,36 @@ class App extends React.Component {
     }
   }
 
-  handleSubmit(event) {
+  handleLogin = async (event) => {
     event.preventDefault();
-  }
+      fetch('https://localhost:5000/customers/login', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              "provider": "username",
+              "data": {
+                  "username": this.state.email,
+                  "password": this.state.password
+              }
+            })
+          }).then((response) => response.json())
+          .then((res) => {
+        if(typeof(res.message) != "undefined"){
+          alert("Error", "Error: "+ res.message);
+        }
+        else{
+          this.setState({ auth_token: res.auth_token });
+          alert("Welcome", " You have succesfully logged in");
+          }
+        }).catch((error) => {
+            console.error(error);
+          });
+    }
 
   render() {
+    console.log (this.state)
     return (
       <form onSubmit={this.handleSubmit}>
       <div className = "container">
@@ -51,15 +76,13 @@ class App extends React.Component {
             required
           />
         </div>
-        <button type="submit" onSubmit={this.handleSubmit}>
-          Login
-        </button>
+        <input type="submit" name = 'submit' value = "Sign In" onSubmit={this.handleLogin}/>
 
         <div className="container">
-          <button type="button" class="cancelbtn">
+          <button type="button" className="cancelbtn">
             Cancel
           </button>
-          <span class="psw">
+          <span className="psw">
             Forgot <a href="https://google.com">password?</a>
           </span>
         </div>
